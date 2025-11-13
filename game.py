5556a819ad0e79b6838356ba83c051a6bc20d8a2
Carson-Hahn <chahn8626@floridapoly.edu>
2025-11-13 00:33:33 +0000

    Complete

--- game.py
+++ game.py
@@ -1,1 +1,7 @@
+import random
+
+choices = ["rock", "paper", "scissors"]
+computer_choice = random.choice(choices)
+
 print("Welcome to Rock-Paper-Scissors!")
+print(f"Computer chose: {computer_choice}")
